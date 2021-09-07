599684cba163b8e1110191cfd880e8bbc07b8535
subhash <81804146+skajmera@users.noreply.github.com>
2021-09-07 19:29:17 +0530

    Create axios_fetch.js

---
+++ all_code/nodejs_basic/axios_fetch.js
@@ -0,0 +1,20 @@
+// const fetch = require('cross-fetch');	//npm install node-fetch
+// fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+//   .then(res => res.json())
+//   .then(json => {
+//       console.log(json);
+//   })
+//   .catch(err => console.log(err));
+// // ////////////////////////////////////
+
+///////////////////////////////////
+// const axios=require('axios')
+// axios.get("https://pokeapi.co/api/v2/pokemon/ditto")
+//   .then((response) => {
+//       console.log(response.data);
+//   })
+//   .catch((error) => {
+//       console.log(error);
+//   })
+
+//////////////////////////////////////////
